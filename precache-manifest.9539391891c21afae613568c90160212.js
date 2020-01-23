9423be4c0505de52fe5d7700891251d4bdf6bb57
dasturi <oba93@hotmail.com>
2020-01-23 22:16:03 +0100

    Updates

---
+++ precache-manifest.9539391891c21afae613568c90160212.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8bb06431eb21ecf21a20807cc3c3fce8",
+    "url": "/oba14/weather-api-rr/index.html"
+  },
+  {
+    "revision": "8836f19a67b23caccc13",
+    "url": "/oba14/weather-api-rr/static/css/2.82caf9b5.chunk.css"
+  },
+  {
+    "revision": "0e6a52fed4188adcb62b",
+    "url": "/oba14/weather-api-rr/static/css/main.a76dad30.chunk.css"
+  },
+  {
+    "revision": "8836f19a67b23caccc13",
+    "url": "/oba14/weather-api-rr/static/js/2.479fea96.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/oba14/weather-api-rr/static/js/2.479fea96.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0e6a52fed4188adcb62b",
+    "url": "/oba14/weather-api-rr/static/js/main.9e1acbba.chunk.js"
+  },
+  {
+    "revision": "d2836c9a244069287b9a",
+    "url": "/oba14/weather-api-rr/static/js/runtime-main.3f830d34.js"
+  }
+]);
